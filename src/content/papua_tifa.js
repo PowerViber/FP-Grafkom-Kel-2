ff1ddd400f601ed3158e39163a5c1a195af7eea7
Justin Chow <justin.chow05@gmail.com>
2025-12-15 17:47:50 +0700

    feat: add papua contents

---
+++ src/content/papua_tifa.js
@@ -0,0 +1,11 @@
+const html = (s) => s[0];
+
+export default html`
+  <h3 class="text-2xl font-semibold mb-4">Tifa</h3>
+  <p>Alat musik pukul tradisional khas Papua dan Maluku.</p>
+  <p>
+    Berbeda dengan di Maluku, Tifa Papua biasanya memiliki pegangan di samping
+    dan dihiasi ukiran rumit yang menceritakan kisah suku pembuatnya. Digunakan
+    untuk mengiringi tarian perang dan upacara adat.
+  </p>
+`;
